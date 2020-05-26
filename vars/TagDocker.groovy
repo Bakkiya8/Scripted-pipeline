a48904061771572388e7ad1bd9ddd43c58e7e1ec
Bakkiya8 <52496334+Bakkiya8@users.noreply.github.com>
2020-05-26 21:32:45 +0530

    Update TagDocker.groovy

--- vars/TagDocker.groovy
+++ vars/TagDocker.groovy
@@ -1,4 +1,4 @@
-def Builddocker(Map imgname=[:],Closure body={})
+def call(Map imgname=[:],Closure body={})
 {
  // def dockerImage = docker.build("${imgname.imagename}:${env.BUILD_ID}")
      // dockerImage.push()
