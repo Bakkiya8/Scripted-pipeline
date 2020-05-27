79bd8cb74e61d25145f998abe1ab67ff5a08cf3d
Bakkiya8 <52496334+Bakkiya8@users.noreply.github.com>
2020-05-27 13:37:04 +0530

    Update TagDocker.groovy

--- vars/TagDocker.groovy
+++ vars/TagDocker.groovy
@@ -1,11 +1,11 @@
 def call(Map imgname=[:],Closure body={})
 {
- // def dockerImage = docker.build("${imgname.imagename}:${env.BUILD_ID}")
-     // dockerImage.push()
  def String cmd = " docker build -t ${imgname.imagename}:${BUILD_NUMBER} ." 
  sh cmd
  echo "Docker Image created"
- sh "docker run -d -p 8050:8050 --name springbootApp ${imgname.imagename}:$BUILD_NUMBER"
+ sh "docker rmi -f ${imgname.contname} "
+ echo "Removed existing image"
+ sh "docker run -d -p  ${imgname.port1}: ${imgname.containerport} --name  ${imgname.contname} ${imgname.imagename}:$BUILD_NUMBER"
  echo "Docker container created"
  body()
 }
