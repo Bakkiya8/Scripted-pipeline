706c2d8921180ab7d4c6e04bc08ada2da433d2b1
Bakkiya8 <52496334+Bakkiya8@users.noreply.github.com>
2020-05-27 14:20:02 +0530

    Update TagDocker.groovy

--- vars/TagDocker.groovy
+++ vars/TagDocker.groovy
@@ -3,7 +3,8 @@ def call(Map imgname=[:],Closure body={})
  def String cmd = " docker build -t ${imgname.imagename}:${BUILD_NUMBER} ." 
  sh cmd
  echo "Docker Image created"
- sh "docker stop $(docker ps -q) || docker rm $(docker ps -a -q) || docker rmi $(docker images -q -f dangling=true)"
+ def stopcontainer = "docker stop $(docker ps -q) || docker rm $(docker ps -a -q) || docker rmi $(docker images -q -f dangling=true)"
+ sh stopcontainer
  echo "Removed existing image"
  sh "docker run -d -p  ${imgname.port1}:${imgname.containerport} --name  ${imgname.contname} ${imgname.imagename}:$BUILD_NUMBER"
  echo "Docker container created"
