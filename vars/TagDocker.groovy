22472abb52b9a2bae9a5524c9949fc93fcede861
Bakkiya8 <52496334+Bakkiya8@users.noreply.github.com>
2020-05-27 13:43:31 +0530

    Update TagDocker.groovy

--- vars/TagDocker.groovy
+++ vars/TagDocker.groovy
@@ -5,7 +5,7 @@ def call(Map imgname=[:],Closure body={})
  echo "Docker Image created"
  sh "docker rmi -f ${imgname.contname} "
  echo "Removed existing image"
- sh "docker run -d -p  ${imgname.port1}: ${imgname.containerport} --name  ${imgname.contname} ${imgname.imagename}:$BUILD_NUMBER"
+ sh "docker run -d -p  ${imgname.port1}:${imgname.containerport} --name  ${imgname.contname} ${imgname.imagename}:$BUILD_NUMBER"
  echo "Docker container created"
  body()
 }
